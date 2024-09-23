953d016d709c702ed2b1c4cdec0c1ee4a9a17466
Grey Wind <wjunqin7@gmail.com>
2024-09-23 11:12:22 +0800

    文件读取与错误模块

---
+++ TTYPDB/Basic/Exception.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TTYPDB.Basic
+{
+#if NET8_0
+    /// <summary>
+    /// The main error module, most of them use this<br/>
+    /// 主要的错误模块，大部分都使用这个
+    /// </summary>
+    public class TtypdbException(string message) : Exception(message) { }
+    /// <summary>
+    /// File opening failure<br/>
+    /// 文件打开失败
+    /// </summary>
+    public class FileOpenFailException(string message) : TtypdbException(message) { }
+    /// <summary>
+    /// File read failure<br/>
+    /// 文件读取失败
+    /// </summary>
+    public class FileReadingFailException(string message) : TtypdbException(message) { }
+    /// <summary>
+    /// Incorrect or invalid data<br/>
+    /// 错误或无效的数据
+    /// </summary>
+    public class InvalidDataException(string message) : TtypdbException(message) { }
+#else
+    /// <summary>
+    /// The main error module, most of them use this<br/>
+    /// 主要的错误模块，大部分都使用这个
+    /// </summary>
+    public class TtypdbException : Exception
+    {
+        public TtypdbException(string message) : base(message) { }
+    }
+    /// <summary>
+    /// File opening failure<br/>
+    /// 文件打开失败
+    /// </summary>
+    public class FileOpenFailException : TtypdbException
+    {
+        public FileOpenFailException(string message) : base(message) { }
+    }
+    /// <summary>
+    /// File read failure<br/>
+    /// 文件读取失败
+    /// </summary>
+    public class FileReadingFailException : TtypdbException
+    {
+        public FileReadingFailException(string message) : base(message) { }
+    }
+    /// <summary>
+    /// Incorrect or invalid data<br/>
+    /// 错误或无效的数据
+    /// </summary>
+    public class InvalidDataException : TtypdbException
+    {
+        public InvalidDataException(string message) : base(message) { }
+    }
+#endif
+}
---
+++ TTYPDB/TTYPDB.cs
@@ -0,0 +1,16 @@
+﻿using TTYPDB.Utils;
+
+namespace TTYPDB
+{
+    public class TTYPDB
+    {
+        public string filePath;
+
+        private string _fileContent;
+
+        public void ReadingFile()
+        {
+            _fileContent = ReadFile.ReadTtypdbFile(filePath);
+        }
+    }
+}
