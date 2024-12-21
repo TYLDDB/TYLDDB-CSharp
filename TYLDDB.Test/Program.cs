e405fca5339d85ab39f083076321be3b29fc154e
Grey Wind <wjunqin7@gmail.com>
2024-12-21 20:36:07 +0800

    docs: change some comments

--- TYLDDB.Test/Program.cs
+++ TYLDDB.Test/Program.cs
@@ -5,7 +5,7 @@ using TYLDDB.Test;
 LDDB lddb = new LDDB();
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////// 读取文件
-HighPrecisionTimer readDbTimer = new(); // 从发起读取文件指令到成功读取的总时间
+HighPrecisionTimer readDbTimer = new(); // 从发起读取文件到成功读取的总时间
 readDbTimer.Start();
 lddb.FilePath = "./example.lddb";
 lddb.ReadingFile();
@@ -13,7 +13,7 @@ readDbTimer.Stop();
 WriteTime("从发起读取文件指令到成功读取的总时间为: ", readDbTimer.ElapsedMilliseconds());
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////// 读取数据库
-HighPrecisionTimer loadDbTimer = new(); // 从发起读取数据库指令到成功返回读取内容的总时间
+HighPrecisionTimer loadDbTimer = new(); // 从发起读取数据库到成功返回读取内容的总时间
 loadDbTimer.Start();
 lddb.LoadDatabase("database1");
 Console.WriteLine(lddb.GetLoadingDatabaseContent()); // 输出database1内容
@@ -21,7 +21,7 @@ loadDbTimer.Stop();
 WriteTime("从发起读取数据库指令到成功返回读取内容的总时间为: ", loadDbTimer.ElapsedMilliseconds());
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////// 获取所有数据库名称
-HighPrecisionTimer readAllDbNameTimer = new(); // 从发起读取数据库指令到成功返回读取内容的总时间
+HighPrecisionTimer readAllDbNameTimer = new(); // 从发起读取数据库名称到成功返回读取内容的总时间
 readAllDbNameTimer.Start();
 lddb.ReadAllDatabaseName();
 readAllDbNameTimer.Stop();
@@ -32,7 +32,7 @@ if(lddb.AllDatabaseName != null)
         Console.WriteLine(dbName);
     }
 }
-WriteTime("从发起读取数据库指令到成功返回读取内容的总时间为: ", readAllDbNameTimer.ElapsedMilliseconds());
+WriteTime("从发起读取数据库名称到成功返回读取内容的总时间为: ", readAllDbNameTimer.ElapsedMilliseconds());
 
 
 
