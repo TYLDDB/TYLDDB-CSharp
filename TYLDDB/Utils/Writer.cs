d815c530b3b8998253aac17246521dd370b0ddbb
Grey Wind <wjunqin7@gmail.com>
2024-12-24 17:22:07 +0800

    重命名文件

---
+++ TYLDDB/Utils/Writer.cs
@@ -0,0 +1,10 @@
+﻿namespace TYLDDB.Utils
+{
+    /// <summary>
+    /// A class used to write contents into the file<br />
+    /// 用于写入文件的类 
+    /// </summary>
+    public class Writer
+    {
+    }
+}
