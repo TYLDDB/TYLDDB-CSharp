e4285f9b29a0ccfa36dbfaa660f850e3c7e4d98d
Grey Wind <wjunqin7@gmail.com>
2025-01-18 13:54:07 +0800

    feat: 添加更多实例化重载

--- TYLDDB/TYLDDB_Instantiation.cs
+++ TYLDDB/TYLDDB_Instantiation.cs
@@ -41,5 +41,189 @@ namespace TYLDDB
             stlBooleanDictionary = new StlBooleanDictionary();
             #endregion
         }
+
+        /// <summary>
+        /// Instantiate the LDDB class<br />
+        /// 实例化LDDB类
+        /// </summary>
+        public LDDB(CacheMode mode)
+        {
+            #region 实例化数据库操作类
+            database_v1 = new Database_V1();
+            database_v2 = new Database_V2();
+            #endregion
+
+            switch (mode)
+            {
+                case CacheMode.ConcurrentDictionary:
+                    #region 实例化并发词典
+                    cdStringDictionary = new CdStringDictionary();
+                    cdShortDictionary = new CdShortDictionary();
+                    cdLongDictionary = new CdLongDictionary();
+                    cdIntegerDictionary = new CdIntegerDictionary();
+                    cdFloatDictionary = new CdFloatDictionary();
+                    cdDoubleDictionary = new CdDoubleDictionary();
+                    cdDecimalDictionary = new CdDecimalDictionary();
+                    cdCharDictionary = new CdCharDictionary();
+                    cdBooleanDictionary = new CdBooleanDictionary();
+                    #endregion
+                    break;
+                case CacheMode.SemaphoreThreadLock:
+                    #region 实例化信号词典
+                    stlStringDictionary = new StlStringDictionary();
+                    stlShortDictionary = new StlShortDictionary();
+                    stlLongDictionary = new StlLongDictionary();
+                    stlIntegerDictionary = new StlIntegerDictionary();
+                    stlFloatDictionary = new StlFloatDictionary();
+                    stlDoubleDictionary = new StlDoubleDictionary();
+                    stlDecimalDictionary = new StlDecimalDictionary();
+                    stlCharDictionary = new StlCharDictionary();
+                    stlBooleanDictionary = new StlBooleanDictionary();
+                    #endregion
+                    break;
+                case CacheMode.CDAndSTL:
+                    #region 实例化并发词典
+                    cdStringDictionary = new CdStringDictionary();
+                    cdShortDictionary = new CdShortDictionary();
+                    cdLongDictionary = new CdLongDictionary();
+                    cdIntegerDictionary = new CdIntegerDictionary();
+                    cdFloatDictionary = new CdFloatDictionary();
+                    cdDoubleDictionary = new CdDoubleDictionary();
+                    cdDecimalDictionary = new CdDecimalDictionary();
+                    cdCharDictionary = new CdCharDictionary();
+                    cdBooleanDictionary = new CdBooleanDictionary();
+                    #endregion
+
+                    #region 实例化信号词典
+                    stlStringDictionary = new StlStringDictionary();
+                    stlShortDictionary = new StlShortDictionary();
+                    stlLongDictionary = new StlLongDictionary();
+                    stlIntegerDictionary = new StlIntegerDictionary();
+                    stlFloatDictionary = new StlFloatDictionary();
+                    stlDoubleDictionary = new StlDoubleDictionary();
+                    stlDecimalDictionary = new StlDecimalDictionary();
+                    stlCharDictionary = new StlCharDictionary();
+                    stlBooleanDictionary = new StlBooleanDictionary();
+                    #endregion
+                    break;
+                case CacheMode.TripleDictionaryCache:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Instantiate the LDDB class<br />
+        /// 实例化LDDB类
+        /// </summary>
+        public LDDB(DatabaseProcess process)
+        {
+            #region 实例化并发词典
+            cdStringDictionary = new CdStringDictionary();
+            cdShortDictionary = new CdShortDictionary();
+            cdLongDictionary = new CdLongDictionary();
+            cdIntegerDictionary = new CdIntegerDictionary();
+            cdFloatDictionary = new CdFloatDictionary();
+            cdDoubleDictionary = new CdDoubleDictionary();
+            cdDecimalDictionary = new CdDecimalDictionary();
+            cdCharDictionary = new CdCharDictionary();
+            cdBooleanDictionary = new CdBooleanDictionary();
+            #endregion
+
+            #region 实例化信号词典
+            stlStringDictionary = new StlStringDictionary();
+            stlShortDictionary = new StlShortDictionary();
+            stlLongDictionary = new StlLongDictionary();
+            stlIntegerDictionary = new StlIntegerDictionary();
+            stlFloatDictionary = new StlFloatDictionary();
+            stlDoubleDictionary = new StlDoubleDictionary();
+            stlDecimalDictionary = new StlDecimalDictionary();
+            stlCharDictionary = new StlCharDictionary();
+            stlBooleanDictionary = new StlBooleanDictionary();
+            #endregion
+
+            switch (process)
+            {
+                case DatabaseProcess.V1:
+                    database_v1 = new Database_V1();
+                    break;
+                case DatabaseProcess.V2:
+                    database_v2 = new Database_V2();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Instantiate the LDDB class<br />
+        /// 实例化LDDB类
+        /// </summary>
+        public LDDB(CacheMode mode, DatabaseProcess process)
+        {
+            switch (mode)
+            {
+                case CacheMode.ConcurrentDictionary:
+                    #region 实例化并发词典
+                    cdStringDictionary = new CdStringDictionary();
+                    cdShortDictionary = new CdShortDictionary();
+                    cdLongDictionary = new CdLongDictionary();
+                    cdIntegerDictionary = new CdIntegerDictionary();
+                    cdFloatDictionary = new CdFloatDictionary();
+                    cdDoubleDictionary = new CdDoubleDictionary();
+                    cdDecimalDictionary = new CdDecimalDictionary();
+                    cdCharDictionary = new CdCharDictionary();
+                    cdBooleanDictionary = new CdBooleanDictionary();
+                    #endregion
+                    break;
+                case CacheMode.SemaphoreThreadLock:
+                    #region 实例化信号词典
+                    stlStringDictionary = new StlStringDictionary();
+                    stlShortDictionary = new StlShortDictionary();
+                    stlLongDictionary = new StlLongDictionary();
+                    stlIntegerDictionary = new StlIntegerDictionary();
+                    stlFloatDictionary = new StlFloatDictionary();
+                    stlDoubleDictionary = new StlDoubleDictionary();
+                    stlDecimalDictionary = new StlDecimalDictionary();
+                    stlCharDictionary = new StlCharDictionary();
+                    stlBooleanDictionary = new StlBooleanDictionary();
+                    #endregion
+                    break;
+                case CacheMode.CDAndSTL:
+                    #region 实例化并发词典
+                    cdStringDictionary = new CdStringDictionary();
+                    cdShortDictionary = new CdShortDictionary();
+                    cdLongDictionary = new CdLongDictionary();
+                    cdIntegerDictionary = new CdIntegerDictionary();
+                    cdFloatDictionary = new CdFloatDictionary();
+                    cdDoubleDictionary = new CdDoubleDictionary();
+                    cdDecimalDictionary = new CdDecimalDictionary();
+                    cdCharDictionary = new CdCharDictionary();
+                    cdBooleanDictionary = new CdBooleanDictionary();
+                    #endregion
+
+                    #region 实例化信号词典
+                    stlStringDictionary = new StlStringDictionary();
+                    stlShortDictionary = new StlShortDictionary();
+                    stlLongDictionary = new StlLongDictionary();
+                    stlIntegerDictionary = new StlIntegerDictionary();
+                    stlFloatDictionary = new StlFloatDictionary();
+                    stlDoubleDictionary = new StlDoubleDictionary();
+                    stlDecimalDictionary = new StlDecimalDictionary();
+                    stlCharDictionary = new StlCharDictionary();
+                    stlBooleanDictionary = new StlBooleanDictionary();
+                    #endregion
+                    break;
+                case CacheMode.TripleDictionaryCache:
+                    break;
+            }
+
+            switch (process)
+            {
+                case DatabaseProcess.V1:
+                    database_v1 = new Database_V1();
+                    break;
+                case DatabaseProcess.V2:
+                    database_v2 = new Database_V2();
+                    break;
+            }
+        }
     }
 }
