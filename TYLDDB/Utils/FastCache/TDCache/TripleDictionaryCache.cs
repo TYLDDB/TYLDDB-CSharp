78fcc04f5512703d28c9b2a962eea00fa0535c0b
Grey Wind <wjunqin7@gmail.com>
2025-01-18 23:24:30 +0800

    feat: 删除 Semaphore Slim
    并发词典不需要搞这种，真的是傻了
    太晚了，脑子都糊涂了

--- TYLDDB/Utils/FastCache/TDCache/TripleDictionaryCache.cs
+++ TYLDDB/Utils/FastCache/TDCache/TripleDictionaryCache.cs
@@ -12,7 +12,6 @@ namespace TYLDDB.Utils.FastCache.TDCache
     public class TripleDictionaryCache : ITripleDictionaryCache, IDisposable
     {
         private TripleDictionary<object> dictionary;
-        private readonly SemaphoreSlim semaphore;  // 控制并发访问
 
         /// <summary>
         /// Three-valued dictionary cache, based on concurrent dictionary as core, combined with semaphore based thread lock to achieve high concurrency stability.<br />
@@ -21,7 +20,6 @@ namespace TYLDDB.Utils.FastCache.TDCache
         public TripleDictionaryCache()
         {
             dictionary = new TripleDictionary<object>();
-            semaphore = new SemaphoreSlim(1, 1);  // 使用信号量来同步
         }
 
         /// <summary>
@@ -37,13 +35,7 @@ namespace TYLDDB.Utils.FastCache.TDCache
         /// Clear all cache entries.<br />
         /// 清空所有缓存项。
         /// </summary>
-        public void Clear()
-        {
-            lock (semaphore)
-            {
-                dictionary = new TripleDictionary<object>();
-            }
-        }
+        public void Clear() => dictionary = new TripleDictionary<object>();
 
         /// <summary>
         /// Synchronization method: Obtain the corresponding value by key.<br />
@@ -52,13 +44,7 @@ namespace TYLDDB.Utils.FastCache.TDCache
         /// <param name="type">Data type.<br />数据类型。</param>
         /// <param name="key">Key<br />键</param>
         /// <returns>Value<br />值</returns>
-        public object Get(string type, string key)
-        {
-            lock (semaphore)
-            {
-                return dictionary.Get(type, key);
-            }
-        }
+        public object Get(string type, string key) => dictionary.Get(type, key);
 
         /// <summary>
         /// Remove a cache entry by its key.<br />
@@ -67,13 +53,7 @@ namespace TYLDDB.Utils.FastCache.TDCache
         /// <param name="type">Data type.<br />数据类型。</param>
         /// <param name="key">Key<br />键</param>
         /// <returns>Whether the removal is successful.<br />移除操作是否成功。</returns>
-        public bool Remove(string type, string key)
-        {
-            lock (semaphore)
-            {
-                return dictionary.RemoveKey(type, key);
-            }
-        }
+        public bool Remove(string type, string key) => dictionary.RemoveKey(type, key);
 
         /// <summary>
         /// Set a cache entry for a specified key.<br />
@@ -83,13 +63,7 @@ namespace TYLDDB.Utils.FastCache.TDCache
         /// <param name="key">Key<br />键</param>
         /// <param name="value">Value<br />值</param>
         /// <returns>Whether the operation is successful.<br />操作是否成功。</returns>
-        public bool Set(string type, string key, object value)
-        {
-            lock (semaphore)
-            {
-                return dictionary.Add(type, key, value);
-            }
-        }
+        public bool Set(string type, string key, object value) => dictionary.Add(type, key, value);
     }
 }
 #endif
