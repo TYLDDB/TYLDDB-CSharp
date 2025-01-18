06377eb5f2bb561b08e610019de3025542798001
Grey Wind <wjunqin7@gmail.com>
2025-01-18 14:12:20 +0800

    test: 优化旧的测试代码

--- TYLDDB.Test/Program.cs
+++ TYLDDB.Test/Program.cs
@@ -9,117 +9,123 @@ List<string> testData = [];
 var lddb = new LDDB();
 #endregion
 
-#region 读取文件
-///////////////////////////////////////////////////////////////////////////////////////////////////////// 读取文件
-HighPrecisionTimer readDbTimer = new(); // 从发起读取文件到成功读取的总时间
-lddb.FilePath = dbFilePath;
-readDbTimer.Start();
-lddb.ReadingFile();
-readDbTimer.Stop();
-WriteTime("从发起读取文件指令到成功读取的总时间为: ", readDbTimer.ElapsedMilliseconds());
-#endregion
+ReadFile();
 
-#region 读取数据库
-///////////////////////////////////////////////////////////////////////////////////////////////////////// 读取数据库
-HighPrecisionTimer loadDbTimer = new(); // 从发起读取数据库到成功返回读取内容的总时间
-loadDbTimer.Start();
-lddb.LoadDatabase("database1");
-Console.WriteLine(lddb.GetLoadingDatabaseContent()); // 输出database1内容
-loadDbTimer.Stop();
-WriteTime("从发起读取数据库指令到成功返回读取内容的总时间为(V1): ", loadDbTimer.ElapsedMilliseconds());
+ReadDatabase();
 
-HighPrecisionTimer loadDbV2Timer = new(); // 从发起读取数据库到成功返回读取内容的总时间
-loadDbV2Timer.Start();
-lddb.LoadDatabase_V2("database1");
-Console.WriteLine(lddb.GetLoadingDatabaseContent()); // 输出database1内容
-loadDbV2Timer.Stop();
-WriteTime("从发起读取数据库指令到成功返回读取内容的总时间为(V2): ", loadDbV2Timer.ElapsedMilliseconds());
-#endregion
+GetAllDatabaseName();
 
-#region 获取所有数据库名称
-///////////////////////////////////////////////////////////////////////////////////////////////////////// 获取所有数据库名称
-HighPrecisionTimer readAllDbNameTimer = new(); // 从发起读取数据库名称到成功返回读取内容的总时间
-readAllDbNameTimer.Start();
-lddb.ReadAllDatabaseName();
-readAllDbNameTimer.Stop();
-if(lddb.AllDatabaseName != null)
-{
-    foreach (var dbName in lddb.AllDatabaseName)
-    {
-        Console.WriteLine(dbName);
-    }
-}
-WriteTime("从发起读取数据库名称到成功返回读取内容的总时间为: ", readAllDbNameTimer.ElapsedMilliseconds());
-#endregion
+ParseDatabaseToTemp();
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////// 数据库解析缓存
-HighPrecisionTimer parseDbTimer = new();
-parseDbTimer.Start();
-await lddb.Parse_V1();
-parseDbTimer.Stop();
-WriteTime("解析文件并写入缓存V1(同步): ", parseDbTimer.ElapsedMilliseconds());
-HighPrecisionTimer parseDbTimerAsync = new();
-parseDbTimerAsync.Start();
-await lddb.ParseAsync_V1();
-parseDbTimerAsync.Stop();
-WriteTime("解析文件并写入缓存V1(异步): ", parseDbTimerAsync.ElapsedMilliseconds());
+CdAllTypeSearch();
 
-//HighPrecisionTimer parseDbV2Timer = new();
-//parseDbV2Timer.Start();
-//lddb.Parse_V2();
-//parseDbV2Timer.Stop();
-//WriteTime("解析文件并写入缓存V1(同步): ", parseDbV2Timer.ElapsedMilliseconds());
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////// 并发词典数据库全类型同步搜寻
-HighPrecisionTimer allTypeSearchFromConcurrentDictionaryTimer = new();
-allTypeSearchFromConcurrentDictionaryTimer.Start();
-string[] AllTypeSearchFromConcurrentDictionaryResult = lddb.AllTypeSearchFromConcurrentDictionary("str_name");
-allTypeSearchFromConcurrentDictionaryTimer.Stop();
-// 使用 foreach 输出数组的每个元素
-foreach (var str in AllTypeSearchFromConcurrentDictionaryResult)
-{
-    Console.WriteLine(str);
-}
-WriteTime("并发词典数据库全类型同步搜寻: ", allTypeSearchFromConcurrentDictionaryTimer.ElapsedMilliseconds());
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////// 信号量线程锁词典数据库全类型同步搜寻
-HighPrecisionTimer allTypeSearchFromSemaphoreThreadLockTimer = new();
-allTypeSearchFromSemaphoreThreadLockTimer.Start();
-string[] AllTypeSearchFromSemaphoreThreadLockResult = lddb.AllTypeSearchFromSemaphoreThreadLock("str_name");
-allTypeSearchFromSemaphoreThreadLockTimer.Stop();
-// 使用 foreach 输出数组的每个元素
-foreach (var str in AllTypeSearchFromSemaphoreThreadLockResult)
-{
-    Console.WriteLine(str);
-}
-WriteTime("信号量线程锁词典数据库全类型同步搜寻: ", allTypeSearchFromSemaphoreThreadLockTimer.ElapsedMilliseconds());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+StlAllTypeSearch();
 
 
 ExportTestData();
 Console.ReadLine();
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////// Test Method
+#region Test Method
+void ReadFile()
+{
+    #region 读取文件
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////// 读取文件
+    HighPrecisionTimer readDbTimer = new(); // 从发起读取文件到成功读取的总时间
+    lddb.FilePath = dbFilePath;
+    readDbTimer.Start();
+    lddb.ReadingFile();
+    readDbTimer.Stop();
+    WriteTime("从发起读取文件指令到成功读取的总时间为: ", readDbTimer.ElapsedMilliseconds());
+    #endregion
+}
+
+void ReadDatabase()
+{
+    #region 读取数据库
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////// 读取数据库
+    HighPrecisionTimer loadDbTimer = new(); // 从发起读取数据库到成功返回读取内容的总时间
+    loadDbTimer.Start();
+    lddb.LoadDatabase("database1");
+    Console.WriteLine(lddb.GetLoadingDatabaseContent()); // 输出database1内容
+    loadDbTimer.Stop();
+    WriteTime("从发起读取数据库指令到成功返回读取内容的总时间为(V1): ", loadDbTimer.ElapsedMilliseconds());
+
+    HighPrecisionTimer loadDbV2Timer = new(); // 从发起读取数据库到成功返回读取内容的总时间
+    loadDbV2Timer.Start();
+    lddb.LoadDatabase_V2("database1");
+    Console.WriteLine(lddb.GetLoadingDatabaseContent()); // 输出database1内容
+    loadDbV2Timer.Stop();
+    WriteTime("从发起读取数据库指令到成功返回读取内容的总时间为(V2): ", loadDbV2Timer.ElapsedMilliseconds());
+    #endregion
+}
+
+void GetAllDatabaseName()
+{
+    #region 获取所有数据库名称
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////// 获取所有数据库名称
+    HighPrecisionTimer readAllDbNameTimer = new(); // 从发起读取数据库名称到成功返回读取内容的总时间
+    readAllDbNameTimer.Start();
+    lddb.ReadAllDatabaseName();
+    readAllDbNameTimer.Stop();
+    if (lddb.AllDatabaseName != null)
+    {
+        foreach (var dbName in lddb.AllDatabaseName)
+        {
+            Console.WriteLine(dbName);
+        }
+    }
+    WriteTime("从发起读取数据库名称到成功返回读取内容的总时间为: ", readAllDbNameTimer.ElapsedMilliseconds());
+    #endregion
+}
+
+async void ParseDatabaseToTemp()
+{
+    #region 数据库解析缓存
+    HighPrecisionTimer parseDbTimer = new();
+    parseDbTimer.Start();
+    await lddb.Parse_V1();
+    parseDbTimer.Stop();
+    WriteTime("解析文件并写入缓存V1(同步): ", parseDbTimer.ElapsedMilliseconds());
+    HighPrecisionTimer parseDbTimerAsync = new();
+    parseDbTimerAsync.Start();
+    await lddb.ParseAsync_V1();
+    parseDbTimerAsync.Stop();
+    WriteTime("解析文件并写入缓存V1(异步): ", parseDbTimerAsync.ElapsedMilliseconds());
+    #endregion
+}
+
+void CdAllTypeSearch()
+{
+    #region 并发词典数据库全类型搜寻
+    HighPrecisionTimer allTypeSearchFromConcurrentDictionaryTimer = new();
+    allTypeSearchFromConcurrentDictionaryTimer.Start();
+    string[] AllTypeSearchFromConcurrentDictionaryResult = lddb.AllTypeSearchFromConcurrentDictionary("str_name");
+    allTypeSearchFromConcurrentDictionaryTimer.Stop();
+    // 使用 foreach 输出数组的每个元素
+    foreach (var str in AllTypeSearchFromConcurrentDictionaryResult)
+    {
+        Console.WriteLine(str);
+    }
+    WriteTime("并发词典数据库全类型同步搜寻: ", allTypeSearchFromConcurrentDictionaryTimer.ElapsedMilliseconds());
+    #endregion
+}
+
+void StlAllTypeSearch()
+{
+    #region 信号量线程锁词典数据库全类型搜寻
+    HighPrecisionTimer allTypeSearchFromSemaphoreThreadLockTimer = new();
+    allTypeSearchFromSemaphoreThreadLockTimer.Start();
+    string[] AllTypeSearchFromSemaphoreThreadLockResult = lddb.AllTypeSearchFromSemaphoreThreadLock("str_name");
+    allTypeSearchFromSemaphoreThreadLockTimer.Stop();
+    // 使用 foreach 输出数组的每个元素
+    foreach (var str in AllTypeSearchFromSemaphoreThreadLockResult)
+    {
+        Console.WriteLine(str);
+    }
+    WriteTime("信号量线程锁词典数据库全类型搜寻: ", allTypeSearchFromSemaphoreThreadLockTimer.ElapsedMilliseconds());
+    #endregion
+}
+#endregion
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////// 工具
 void WriteTime(string what, double time)
